f1b4d0e366d4be72c0ffa8176184462dd904ebfd
大圣 <yyf42336@gmail.com>
2018-10-18 16:02:41 +0800

    登陆退出功能

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require bootstrap/alert
+//= require bootstrap/dropdown
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
